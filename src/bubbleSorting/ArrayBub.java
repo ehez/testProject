b3b897f41c35c0d293288abdbd9b7d4798b0be99
ErickH <ehhernandezh@gmail.com>
2018-09-11 22:15:26 -0400

    First commit

---
+++ src/bubbleSorting/ArrayBub.java
@@ -0,0 +1,45 @@
+package bubbleSorting;
+
+public class ArrayBub {
+	private int[] a;
+	private int nElems;
+
+	public ArrayBub(int maxSize){
+		a = new int[maxSize];
+		nElems = 0;
+	}
+
+	public void insert(int value){
+		a[nElems++] = value;
+	}
+
+	public void display(){
+		for( int i =0; i < nElems; i++){
+			System.out.print(a[i] + " ");
+		}
+	}
+
+	public void bubbleSort(){
+		int in;
+		int out;
+		int numberOfComparisons = 0;
+		int numberOfSwaps =0;
+		for(out = nElems-1; out >=1; out--){
+			for(in = 0; in < out; in++){
+				numberOfComparisons++;
+				if(a[in] > a[in+1]){
+					numberOfSwaps++;
+					swap(in, in+1);
+				}
+			}
+		}
+//		System.out.println();
+		System.out.println("\nComparisons: " + numberOfComparisons +"\nSwaps: "+numberOfSwaps);
+	}
+
+	private void swap(int dex1, int dex2){
+		int temp = a[dex1];
+		a[dex1] = a[dex2];
+		a[dex2] = temp;
+	}
+}
---
+++ src/bubbleSorting/Demo.java
@@ -0,0 +1,40 @@
+package bubbleSorting;
+
+public class Demo {
+	public static void main(String[] args){
+		ArrayBub arr = new ArrayBub(15);
+		
+		
+
+//		arr.insert(9);
+//		arr.insert(8);
+//		arr.insert(7);
+//		arr.insert(6);
+//		arr.insert(5);
+//		arr.insert(4);
+//		arr.insert(3);
+//		arr.insert(2);
+//		arr.insert(1);
+//		arr.insert(0);
+//		arr.insert(-1);
+
+//		arr.insert(1);
+//		arr.insert(2);
+//		arr.insert(3);
+//		arr.insert(4);
+//		arr.insert(5);
+//		arr.insert(6);
+//		arr.insert(7);
+//		arr.insert(8);
+//		arr.insert(9);
+//		arr.insert(10);
+		
+	
+
+		arr.display();
+		long startTime = System.nanoTime();
+		arr.bubbleSort();
+		System.out.println(System.nanoTime() - startTime);
+		arr.display();
+	}
+}
